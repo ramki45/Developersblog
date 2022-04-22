55074c740b17cde4ba9544c48ba6fcd2ab8da41e
Ramki <89958455+ramki45@users.noreply.github.com>
2022-04-22 13:03:10 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const cors = require('cors');
+
+
+dotenv.config();
+app.use(cors());
+app.use(express.json({limit :"30mb", extended : true}));
+app.use(express.urlencoded({limit : "30mb", extended : true}))
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`${PORT} is running`);
+});
